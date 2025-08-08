8a485facd7ded59ef8d9931488314974f1b1228e
shuangli-z <shualee@amazon.com>
2025-08-08 14:11:43 +0800

    use agent for LLM calls and container for insights storage
    Signed-off-by: shuangli-z <shualee@amazon.com>

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/indices/MLIndicesHandler.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/indices/MLIndicesHandler.java
@@ -96,9 +96,7 @@ public class MLIndicesHandler {
         initMLIndexIfAbsent(MLIndex.JOBS, listener);
     }
 
-    public void initMLIndexInsightIndex(ActionListener<Boolean> listener) {
-        initMLIndexIfAbsent(MLIndex.INDEX_INSIGHT, listener);
-    }
+
 
     public void initMLAgentIndex(ActionListener<Boolean> listener) {
         initMLIndexIfAbsent(MLIndex.AGENT, listener);
