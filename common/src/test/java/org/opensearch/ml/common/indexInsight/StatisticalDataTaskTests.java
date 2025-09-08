05725f0df27912692c43cddbf3cc0dbc0e734317
shuangli-z <shualee@amazon.com>
2025-09-08 14:04:05 +0800

    address PR comments: add AbstractIndexInsightTask and remove IndexInsightUtils
    Signed-off-by: shuangli-z <shualee@amazon.com>

--- common/src/test/java/org/opensearch/ml/common/indexInsight/StatisticalDataTaskTests.java
+++ common/src/test/java/org/opensearch/ml/common/indexInsight/StatisticalDataTaskTests.java
@@ -137,7 +137,7 @@ public class StatisticalDataTaskTests {
         Client client = mock(Client.class);
         StatisticalDataTask task = new StatisticalDataTask("test-index", client, sdkClient);
 
-        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expect(IllegalStateException.class);
         exceptionRule.expectMessage("StatisticalDataTask has no prerequisites");
         task.createPrerequisiteTask(MLIndexInsightType.FIELD_DESCRIPTION);
     }
